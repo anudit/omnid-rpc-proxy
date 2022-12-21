bfbc04f6657f4340bb5c05c58cd79ed67dd3b441
Anudit Nagar <nagaranudit@gmail.com>
2022-12-21 05:09:41 +0000

    Improvements

--- install_helios.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-curl https://raw.githubusercontent.com/a16z/helios/master/heliosup/install | bash
-heliosup
-helios --rpc-port 8547 --execution-rpc https://eth-mainnet.g.alchemy.com/v2/jz1AlVEXLDxlzNMcKDg9To03aBnOssyH
---
+++ run_helios.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./helios --rpc-port 8547 --execution-rpc https://eth-mainnet.g.alchemy.com/v2/jz1AlVEXLDxlzNMcKDg9To03aBnOssyH
